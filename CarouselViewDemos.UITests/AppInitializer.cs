e66b61e8f29490dbeaf704ff1c30fbde5db81715
tafuji <tafuji1223@outlook.com>
2021-06-27 18:21:50 +0900

    Added the ui test project

---
+++ CarouselViewDemos.UITests/AppInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CarouselViewDemos.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            // TODO: If the iOS or Android app being tested is included in the solution 
+            // then open the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            //
+            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
+            // installed. To start the Test Cloud Agent the following code should be
+            // added to the FinishedLaunching method of the AppDelegate:
+            //
+            //    #if ENABLE_TEST_CLOUD
+            //    Xamarin.Calabash.Start();
+            //    #endif
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    // TODO: Update this path to point to your Android app and uncomment the
+                    // code if the app is not included in the solution.
+                    //.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+                    .EnableLocalScreenshots()
+                    .InstalledApp("com.companyname.carouselviewdemos")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                // TODO: Update this path to point to your iOS app and uncomment the
+                // code if the app is not included in the solution.
+                //.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/XamarinForms.iOS.app")
+                .InstalledApp("com.companyname.CarouselViewDemos")
+                .DeviceIdentifier("9A2844B6-CD4C-4946-9359-323428AD6273")
+                .StartApp();
+        }
+    }
+}
---
+++ CarouselViewDemos.UITests/Tests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CarouselViewDemos.UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void Repl()
+        {
+            app.Repl();
+        }
+
+
+        [Test]
+        public void ScrollRightCarouselView()
+        {
+            // move to Horizontal layout (data template)
+            string cellClassName;
+            int cellIndex;
+            if(platform == Platform.Android)
+            {
+                cellClassName = "TextCellRenderer_TextCellView";
+                cellIndex = 3;
+            }
+            else
+            {
+                cellClassName = "Xamarin_Forms_Platform_iOS_CellTableViewCell";
+                cellIndex = 2;
+            }
+            app.Tap(x => x.Class(cellClassName).Index(cellIndex));
+
+
+            Func<AppQuery, AppQuery> caroucelViewQuery;
+            string croucelViewClassName;
+            if(platform == Platform.Android)
+            {
+                croucelViewClassName = "CarouselViewRenderer";
+                caroucelViewQuery = x => x.Class(croucelViewClassName).Index(0).Descendant("LabelRenderer").Index(0);
+            }
+            else
+            {
+                croucelViewClassName = "Xamarin_Forms_Platform_iOS_CarouselTemplatedCell";
+                caroucelViewQuery = x => x.Class(croucelViewClassName).Index(0).Class("Xamarin_Forms_Platform_iOS_LabelRenderer").Index(1).Class("UILabel");
+            }
+
+            app.WaitFor(() =>
+            {
+                var foundItem = false;
+                while(!foundItem)
+                {
+                    var carouselItemTitleLabel = app.Query(caroucelViewQuery).FirstOrDefault();
+                    if(carouselItemTitleLabel.Text == "Tonkin Snub-nosed Monkey")
+                    {
+                        foundItem = true;
+                    }
+                    else
+                    {
+                        app.SwipeRightToLeft(x => x.Class(croucelViewClassName).Index(0), swipeSpeed : 10000);
+                    }
+                }
+                return foundItem;
+            }, timeout: TimeSpan.FromMinutes(1));
+        }
+    }
+}
